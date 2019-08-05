70a3c69dc5d38d5a88610f77a75dd696928c1d5e
Jim Cottrell <jim@aspendigital.com>
2019-08-05 16:37:19 -0600

    Update MultiConditional form widget for multi-element fields

--- formwidgets/MultiConditional.php
+++ formwidgets/MultiConditional.php
@@ -3,6 +3,7 @@
 namespace AspenDigital\LookFeel\FormWidgets;
 
 use Backend\Classes\FormWidgetBase;
+use Str;
 
 class MultiConditional extends FormWidgetBase
 {
@@ -21,20 +22,26 @@ class MultiConditional extends FormWidgetBase
         else { // Settings form controller does not use FormController behavior
             $form = $this->controller->widget->form;
         }
-        
+
         $config = $this->formField->getConfig('multiConditional');
-        
+
         if (empty($config['sources'])) {
             $config['sources'] = [];
         }
-        
+
         array_walk($config['sources'], function(&$source) use($form) {
-            $source['field'] = $form->getField($source['field'])->getName();
+            $fieldName = $source['field'];
+            $suffix = '';
+            if (Str::endsWith($fieldName, '[]')) {
+                $fieldName = substr($fieldName, 0, -2);
+                $suffix = '[]';
+            }
+            $source['field'] = $form->getField($fieldName)->getName() . $suffix;
         });
-        
+
         return $this->makePartial('multiconditional', ['field'=>$this->formField, 'config' => $config]);
     }
-    
+
     public function loadAssets()
     {
         $this->addJs('js/multiconditional.js', '1'); // Manual cache busting
